4901ecf650a56a9ae4e38b4cac05cc12368bcfef
ahmiiidjamal <ahmedalsoni190@gmail.com>
2026-01-13 22:14:45 +0300

    Welcome

---
+++ jsfile/main.js
@@ -0,0 +1,14 @@
+// here start code about menu click for mobile
+
+let clickyes = document.getElementById("click-yes")
+let clickno = document.getElementById("click-no")
+let links = document.getElementById("links")
+clickyes.onclick = function(){
+    links.style.right = "-15px"
+}
+clickno.onclick = function(){
+    links.style.right = "-190px"
+}
+
+// click btn in home page to go links guthub facebook instegram
+
